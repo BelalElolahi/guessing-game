8b7dcc29f461ff58a92abc77904513b93fe040e3
BelalElolahi <belalelolahi003@gmail.com>
2021-07-26 14:32:56 -0700

    Lab2 code201 add about me guessing game

---
+++ js/app.js
@@ -0,0 +1,100 @@
+'use strict';
+
+
+let userName = prompt('enter your name please ');
+ alert("hello "+userName+" we ganna ask you 5 questions just for fun and we hope you enjoy^_^ , Your answer must be Yes or No ");
+
+ let question01 = prompt('we using  css  for styling pages of html?');
+ let question1=question01.toLowerCase();
+ if (question1 =='yes' || question1 =='y')
+ {
+  console.log("correct answer");
+  alert('correct answer');
+
+ }else if(question1 == 'no' || question1 == 'n')
+ {
+   alert("wrong answer");
+ } 
+ else {
+     alert('yor answer not include yes or no ')
+ }
+
+ let question02 = prompt('we use html for make pages interact with user?');
+ let question2=question02.toLowerCase();
+ if (question2 == 'yes' || question2 == 'y')
+ {
+    alert("wrong answer");
+     
+ } else if(question2 == 'no' || question2 == 'n')
+ {
+    console.log("correct answer");
+    alert("correct answer");
+ } else {
+    alert('yor answer not include yes or no ')
+}
+
+let question03 = prompt('html stand for hypertext markup language?');
+ let question3=question03.toLowerCase();
+ if (question3 == 'yes' || question3 == 'y')
+ {
+     console.log("correct answer");
+     alert("correct answer");
+
+ } else if(question3 == 'no' || question3 == 'n')
+ {
+     alert("wrong answer");
+ } else {
+    alert('yor answer not include yes or no ')
+}
+
+ let question04 = prompt("javascript is not programming language?");
+ let question4=question04.toLowerCase();
+ if (question4 == 'yes' || question4 == 'y')
+ {
+    alert("wrong answer");
+
+ } else if(question4 == 'no' || question4 == 'n')
+ {
+    console.log("correct answer");
+    alert("correct answer");
+    
+ } else {
+    alert('yor answer not include yes or no ')
+}
+
+ let question05 = prompt("we use javascript to make pages more interact with user?");
+ let question5=question05.toLowerCase();
+ if (question5 == 'yes' || question5 == 'y')
+ {
+     console.log("correct answer");
+     alert("correct answer");
+
+ } else if(question5 == 'no' || question5 == 'n')
+ {
+     alert("wrong answer");
+ } else {
+    alert('yor answer not include yes or no ')
+}
+
+alert("Thank you for your time "+userName +' we hop you enjoy with our questions')
+
+
+ 
+
+ 
+
+ 
+
+
+
+
+    
+
+
+
+  
+
+
+
+
+  
